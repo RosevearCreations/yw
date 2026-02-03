83262cd97da454b881e1b0c063fe2cc2d6b298ad
RosevearCreations <jfrosevear@gmail.com>
2026-02-03 14:09:39 -0500

    Update cache version and format fetch event handler

--- server-worker.js
+++ server-worker.js
@@ -1,14 +1,14 @@
-const CACHE = 'ywi-hse-v5';
+const CACHE = 'ywi-hse-v8';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 self.addEventListener('install', e => {
-e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 self.addEventListener('fetch', e => {
-e.respondWith(
-caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-const copy = res.clone();
-caches.open(CACHE).then(c => c.put(e.request, copy));
-return res;
-}))
-);
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }))
+  );
 });
