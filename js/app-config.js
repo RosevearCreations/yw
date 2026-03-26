f9ceb166ecf02e6de7213c9c3e275897545e03fe
John Rosevear <jfrosevear@gmail.com>
2026-03-25 20:25:47 -0400

    Add files via upload

--- js/app-config.js
+++ js/app-config.js
@@ -8,7 +8,7 @@
 
 window.YWI_RUNTIME_CONFIG = Object.assign({}, window.YWI_RUNTIME_CONFIG || {}, {
   SUPABASE_URL: 'https://jmqvkgiqlimdhcofwkxr.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptcXZrZ2lxbGltZGhjb2Z3a3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMDYzNDYsImV4cCI6MjA4NTU4MjM0Nn0.ULYqX2TL08_wfREPCIZjIbRf8nAc61ZWndm8UUJZ-D4',
+  SUPABASE_ANON_KEY: '',
   APP_ENV: 'production',
   APP_CONFIG_SOURCE: 'js/app-config.js',
   APP_CONFIG_UPDATED_AT: '2026-03-25'
