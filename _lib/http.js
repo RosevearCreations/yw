5309f7f9b008fb8ad773f7497b1d16945adf378e
John Rosevear <jfrosevear@gmail.com>
2026-03-30 18:34:26 -0400

    Delete _lib directory

--- _lib/http.js
+++
@@ -1,1 +0,0 @@
-export function json(data, status = 200) { return new Response(JSON.stringify(data, null, 2), { status, headers: { "Content-Type": "application/json; charset=utf-8", "Cache-Control": "no-store" } }); }
