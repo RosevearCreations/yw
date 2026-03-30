e26b624c4e7ea7f734447175f22b2c6122d97733
John Rosevear <jfrosevear@gmail.com>
2026-03-30 18:37:04 -0400

    Delete sql/2026-03-26_customer_flow_inventory_signatures.sql

--- sql/2026-03-26_customer_flow_inventory_signatures.sql
+++
@@ -1,4 +0,0 @@
--- 2026-03-26_customer_flow_inventory_signatures.sql
-alter table public.catalog_inventory_items
-  add column if not exists purchase_date date null,
-  add column if not exists estimated_jobs_per_unit numeric(12,2) null;
